e3e8cc1d3bd6cbaa2e268d4593a10fa5dd87e20c
jadewang425 <jadewang425@gmail.com>
2023-09-11 01:11:29 -0400

    game complete

--- py-pac-poe-lab.py
+++ py-pac-poe-lab.py
@@ -1,15 +1,16 @@
 
 board = { 
-        'A1': " ",
-        'A2': " ",
-        'A3': " ",
-        'B1': " ",
-        'B2': " ",
-        'B3': " ",
-        'C1': " ",
-        'C2': " ",
-        'C3': " "
-    }
+    'A1': " ",
+    'A2': " ",
+    'A3': " ",
+    'B1': " ",
+    'B2': " ",
+    'B3': " ",
+    'C1': " ",
+    'C2': " ",
+    'C3': " "
+}
+
 board_view = f"""
     A   B   C
     
@@ -25,6 +26,7 @@ opening_msg = """
 Let's play Py-Pac-Poe!
 ----------------------
 """
+
 print(opening_msg)
 print(board_view)
 
@@ -33,15 +35,14 @@ winner = False
 
 # check win or tie function to be called in render_game
 def check_win_or_tie():
-    if board['A1'] == board['A2'] == board['A3'] or board['B1'] == board['B2'] == board['B3'] or board['C1'] == board['C2'] == board['C3'] or board['A1'] == board['B1'] == board['C1'] or board['A2'] == board['B2'] == board['C2'] or board['A3'] == board['B3'] == board['C3'] or board['A1'] == board['B2'] == board['C3'] or board['A3'] == board['B2'] == board['C1']:
-        return winner == cur_player
-    else:
-        board_values = list(board.values())
-        if " " not in board_values:
-            return winner == 'T'
-
+    global winner
+    if cur_player == board['A1'] == board['A2'] == board['A3'] or cur_player == board['B1'] == board['B2'] == board['B3'] or cur_player == board['C1'] == board['C2'] == board['C3'] or cur_player == board['A1'] == board['B1'] == board['C1'] or cur_player == board['A2'] == board['B2'] == board['C2'] or cur_player == board['A3'] == board['B3'] == board['C3'] or cur_player == board['A1'] == board['B2'] == board['C3'] or cur_player == board['A3'] == board['B2'] == board['C1']:
+        winner = cur_player
+    elif " " not in list(board.values()):
+        winner = 'T'
         
 def render_game():
+    global cur_player
     cur_input = input(f"Player {cur_player}'s Move (example B2): ").upper()
     if cur_input not in board:
         print('That is not on the board!')
@@ -49,7 +50,7 @@ def render_game():
         print(f"{board[cur_input]} is already in here!")
     else:
         board[cur_input] = cur_player
-        #
+        global board_view 
         board_view = f"""
     A   B   C
     
@@ -59,6 +60,7 @@ def render_game():
    -----------
 3)  {board['A3']} | {board['B3']} | {board['C3']} 
 """
+
         print(board_view)
         #check winner or tie
         check_win_or_tie()
@@ -67,11 +69,9 @@ def render_game():
                 print("Cats game!")
             else:
                 print(f"{cur_player} wins!")
-        # else:
-        #     if cur_player == 'X':
-        #         cur_player = 'O'
-        #     else:
-        #         cur_player = 'X'
-    render_game()
+        else:
+            print(cur_player)
+            cur_player = 'O' if cur_player == 'X' else 'X'
+            render_game()
 
 render_game()
